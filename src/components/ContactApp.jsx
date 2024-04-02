3286c34f30074a73cff037d53444ccb8cc420afd
Azzaxy <abdurrohmanazis@gmail.com>
2024-04-02 16:48:14 +0700

    feat: protect content is login-register

--- src/components/ContactApp.jsx
+++ src/components/ContactApp.jsx
@@ -1,23 +1,53 @@
+import React from "react";
 import Navigation from "./Navigation";
 import { Routes, Route } from "react-router-dom";
 import AddPage from "../pages/AddPage";
 import HomePageWrapper from "../pages/HomePage";
 
-const ContactApp = () => {
-  return (
-    <div className="contact-app">
-      <header className="contact-app__header">
-        <h1>Aplikasi Kontak</h1>
-        <Navigation />
-      </header>
-      <main>
-        <Routes>
-          <Route path="/" element={<HomePageWrapper />} />
-          <Route path="/add" element={<AddPage />} />
-        </Routes>
-      </main>
-    </div>
-  );
-};
+class ContactApp extends React.Component {
+  constructor(props) {
+    super(props);
+
+    //  inisialisasi state authUser
+    this.state = {
+      authedUser: null,
+    };
+  }
+
+  render() {
+    if (this.state.authedUser === null) {
+      return (
+        <div className="contact-app">
+          <header className="contact-app__header">
+            <h1>Aplikasi Kontak</h1>
+            <Navigation />
+          </header>
+          <main>
+            <Routes>
+              {/* tanda * : seluruh URL yang belum terdefinisikan. */}
+              <Route path="/*" element={<p>Halaman Login</p>} />
+              <Route path="/register" element={<p>Halaman Register</p>} />
+            </Routes>
+          </main>
+        </div>
+      );
+    }
+
+    return (
+      <div className="contact-app">
+        <header className="contact-app__header">
+          <h1>Aplikasi Kontak</h1>
+          <Navigation />
+        </header>
+        <main>
+          <Routes>
+            <Route path="/" element={<HomePageWrapper />} />
+            <Route path="/add" element={<AddPage />} />
+          </Routes>
+        </main>
+      </div>
+    );
+  }
+}
 
 export default ContactApp;
